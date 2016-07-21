49ba82e6f05cbf1e75491a16b9018712f7b0c16d
Trung Ngo <trungthengo1995@gmail.com>
2016-07-21 17:52:47 -0400

    "Add a complete project of Twitter integration & a partially complete project of Facebook integration. Still have some work for figuring out the Facebook integration process"

---
+++ CameraCanvasUI/ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics; 
+using TweetSharp;
+
+namespace TweetingIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterClientInfo twitterClientInfo = new TwitterClientInfo();
+            twitterClientInfo.ConsumerKey = ConsumerKey;
+            twitterClientInfo.ConsumerSecret = ConsumerSecret;
+
+            TwitterService twitterService = new TwitterService(twitterClientInfo); 
+
+            if (string.IsNullOrEmpty(AccessToken) || string.IsNullOrEmpty(AccessTokenSecret))
+            {
+                //First need to access the request token and authorization url link 
+                OAuthRequestToken requestToken = twitterService.GetRequestToken();
+                string authorizationURL = twitterService.GetAuthorizationUri(requestToken).ToString();
+
+                //authorizationURL is a URL, so need to paste it in any web browser 
+                Console.WriteLine("Please allow this app to send tweets on your behalf");
+
+                //authorize the app 
+                Console.WriteLine("Enter the PIN from the browser");
+                string pin = Console.ReadLine();
+
+                //get the access token written by you
+                OAuthAccessToken accessToken = twitterService.GetAccessToken(requestToken);
+                string token = accessToken.Token; //Note: set the breakpoint here
+                string tokenSecret = accessToken.TokenSecret; //another one here
+
+                Console.WriteLine("Write down the accessToken: " + token);
+                Console.WriteLine("Write down the accessTokenSecret: " + tokenSecret);
+            }
+            //otherwise use the existed accessToken and accessTokenSecret
+            twitterService.AuthenticateWith(AccessToken, AccessTokenSecret);
+
+            Console.WriteLine("Enter a tweet:");
+            string tweetMessage = Console.ReadLine();
+            //an API change: SendTweetOptions is kinda a wrapper obj takes mutiple computed properties
+            TwitterStatus twitterStatus = twitterService.SendTweet(new SendTweetOptions { Status = tweetMessage });
+        }
+
+        /*Main theme issue here: ConfigurationManager not existed. Then, need to add
+         * the reference inside the AssemplyInfo file
+         * (idea: http://stackoverflow.com/questions/1274852/the-name-configurationmanager-does-not-exist-in-the-current-context)
+         */
+        private static string ConsumerKey
+        {
+            get { return ConfigurationManager.AppSettings["ConsumerKey"]  ;  } 
+        }
+
+        private static string ConsumerSecret
+        {
+            get { return ConfigurationManager.AppSettings["ConsumerSecret"]; }
+        }
+
+        private static string AccessToken
+        {
+            get { return ConfigurationManager.AppSettings["AccessToken"]; }
+        }
+
+        private static string AccessTokenSecret
+        {
+            get { return ConfigurationManager.AppSettings["AccessTokenSecret"]; }
+        }
+    }
+}
--- CameraCanvasUI/EffectsTools/ColorThresholdingCCButton.Designer.cs
+++ CameraCanvasUI/EffectsTools/ColorThresholdingCCButton.Designer.cs
@@ -1,38 +1,38 @@
-﻿namespace CameraCanvas.EffectsTools
-{
-    partial class ColorThresholdingCCButton
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+﻿////namespace cameracanvas.effectstools
+////{
+////    partial class colorthresholdingccbutton
+////    {
+////        /// <summary>
+////        /// required designer variable.
+////        /// </summary>
+////        private system.componentmodel.icontainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+////        /// <summary>
+////        /// clean up any resources being used.
+////        /// </summary>
+////        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+////        protected override void dispose(bool disposing)
+////        {
+////            if (disposing && (components != null))
+////            {
+////                components.dispose();
+////            }
+////            base.dispose(disposing);
+////        }
 
-        #region Windows Form Designer generated code
+////        #region windows form designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ImageFiltering";
-        }
+////        /// <summary>
+////        /// required method for designer support - do not modify
+////        /// the contents of this method with the code editor.
+////        /// </summary>
+////        private void initializecomponent()
+////        {
+////            this.components = new system.componentmodel.container();
+////            this.autoscalemode = system.windows.forms.autoscalemode.font;
+////            this.text = "imagefiltering";
+////        }
 
-        #endregion
-    }
-}
+////        #endregion
+////    }
+////}
